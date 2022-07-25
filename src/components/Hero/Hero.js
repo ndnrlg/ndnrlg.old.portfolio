1bdbca15ef1f00da8d184e436df5e8b9a84cd748
Nandini Neralagi <nandinineralagi@gmail.com>
2022-07-25 14:33:58 -0700

    Updated README

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { LeftSection, GridContainer, ProfileImage} from './HeroStyles';
-const imageStyles = { maxwidth: '20%', maxheight: '20%'};
 
 const Hero = (props) => (
   <Section row nopadding>
@@ -14,10 +13,7 @@ const Hero = (props) => (
         Statistics + Machine Learning @ Carnegie Mellon   
       </SectionText>
     </LeftSection>
-    <img
-        styles={imageStyles}
-        src="https://media-exp1.licdn.com/dms/image/C5603AQGrBOI_9wHZMw/profile-displayphoto-shrink_800_800/0/1654125972975?e=1664409600&v=beta&t=rYfYINSi6SUBmyiw-upaV2IqRhxGeEgL11UwqY-JjjY"
-      />
+    <ProfileImage src="https://media-exp1.licdn.com/dms/image/C5603AQGrBOI_9wHZMw/profile-displayphoto-shrink_800_800/0/1654125972975?e=1664409600&v=beta&t=rYfYINSi6SUBmyiw-upaV2IqRhxGeEgL11UwqY-JjjY" />
   </Section>
 );
 
--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -10,14 +10,16 @@ export const LeftSection = styled.div`
   }
 `;
 
-export const ProfileImage = styled.div`
-width: 20%;
-padding-right: 15rem;
-@media ${(props) => props.theme.breakpoints.md} {
-  width: 80%;
-  display: flex;
-  flex-direction: column;
-}
+export const ProfileImage = styled.img`
+  width: 55rem;
+  height: 35rem;
+  padding-right: 15rem;
+  padding-left: 5rem;
+  @media ${(props) => props.theme.breakpoints.md} {
+    width: 80%;
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 
